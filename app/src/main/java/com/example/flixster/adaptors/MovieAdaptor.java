f2f3b8c7a2ebf487324ddee5051a4d03363b4c3e
octari <ycjariel@gmail.com>
2020-09-24 21:42:56 -0500

    Fixing the poster

--- app/src/main/java/com/example/flixster/adaptors/MovieAdaptor.java
+++ app/src/main/java/com/example/flixster/adaptors/MovieAdaptor.java
@@ -1,6 +1,7 @@
 package com.example.flixster.adaptors;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -29,12 +30,14 @@ public class MovieAdaptor extends RecyclerView.Adapter<MovieAdaptor.ViewHolder> 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Log.d("MovieAdaptor", "onCreateViewHolder");
         View movieView = LayoutInflater.from(context).inflate(R.layout.item_movie, parent, false);
         return new ViewHolder(movieView);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Log.d("MovieAdaptor", "onBindViewHolder" + position);
         Movie movie = movies.get(position);
         holder.bind(movie);
     }
